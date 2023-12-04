ca61b3cac06740f8df564bd4da142046cac53f65
shushantbk16 <119441991+shushantbk16@users.noreply.github.com>
2023-12-04 19:57:55 +0530

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "16355286534dd016"; }
+/******/ 		__webpack_require__.h = function() { return "971973f980589da1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
